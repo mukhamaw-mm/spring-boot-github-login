8267a55d1668804cd4b44d8f356735fde8d1b065
mukhamaw-mm <mukhamaw@gmail.com>
2022-12-01 14:05:12 +0630

    first commit

---
+++ src/main/java/com/mk/springgithublogin/SpringGithubLoginApplication.java
@@ -0,0 +1,25 @@
+package com.mk.springgithublogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@SpringBootApplication
+@RestController
+public class SpringGithubLoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringGithubLoginApplication.class, args);
+    }
+
+    @RequestMapping("/user")
+    public Mono<String> user(@AuthenticationPrincipal Mono<OAuth2User> principal) {
+        return principal.map(val -> val.getAttribute("login"));
+    }
+
+
+}
